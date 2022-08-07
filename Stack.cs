36b57bf8c4a2004895d863bea7f79c518fec1a2c
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-07 22:31:43 +0300

    Generic Solution file edited

--- Stack.cs
+++ Stack.cs
@@ -3,53 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Stack
-{   public class Stack
-    {   List<object> listOfData;
-        Stack(List<object> list)
+  public class StackClass<T>
+    {   List<T> listOfData;
+        StackClass()
         {
-            this.listOfData = list;
+            listOfData = new List<T>();
         }
-        public  void PushData(object value)
+        public  void PushData(T value)
         {
             listOfData.Add(value);
         }
-        public object PopData()
-        {   try
+        public T? PopData()
+        {
+            if (listOfData.Count > 0)
             {
-                object data = listOfData.ElementAt(listOfData.Count - 1);
+                T data = listOfData.ElementAt(listOfData.Count - 1);
                 listOfData.RemoveAt(listOfData.Count - 1);
                 return data;
             }
-            catch (Exception)
-            {
-                return "The List Is Empty ";
-           }
+            return default(T);
+
         }
 
         public  void PrintData()
         {
             try
             {
-                List<object> data = listOfData.Select(s => s).ToList();
+                List<T> data = listOfData.Select(s => s).ToList();
                 foreach (var item in data)
                 {
                     Console.Write(item + " ");
                 }
             }
-            catch(Exception ) { 
-            
-                Console.WriteLine("The List Is Empty Nothing To Print");
+            catch (Exception)
+            {
+
+                Console.WriteLine("The List Is Empty Nothing To Print ");
             }
         }
         public void PeakValue()
         {
             try
             {
-                Console.WriteLine("Peak Value is: "+listOfData.ElementAt(listOfData.Count - 1));
+                Console.WriteLine("Peak Value is: " + listOfData.ElementAt(listOfData.Count - 1));
             }
-            catch(Exception )
+            catch (Exception)
             {
                 Console.WriteLine("Empty List Nothing To Print");
             }
@@ -60,22 +58,16 @@ namespace Stack
             Console.WriteLine("List is Cleared");
 
         }
-
-        public static void Main(string[] args)
-        {   
-            Stack stack1 = new Stack(new List<object>());
-            stack1.PushData(5);
-            stack1.PushData("data");
-            stack1.PushData(3.5);
-            Console.WriteLine(stack1.PopData());
-            ///stack1.PrintData();
-            stack1.PeakValue();
-            stack1.ClearList();
-            stack1.PeakValue();
-        }
-
-    
-
-
+    public static void Main(string[] args)
+    {
+        StackClass<string> stack1 = new StackClass<string>();
+        stack1.PushData("data1");
+        /*     
+                  Console.WriteLine(stack1.PopData());
+                  ///stack1.PrintData();
+                  stack1.PeakValue();
+                  stack1.ClearList();
+                  stack1.PeakValue();*/
     }
 }
+
